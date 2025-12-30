8b0330c3b290cc6fb9a121501dc1ff6f98d41a79
mysaelsantos <167044211+mysaelsantos@users.noreply.github.com>
2025-12-30 14:24:48 -0300

    Update App.tsx

--- curriculo/App.tsx
+++ curriculo/App.tsx
@@ -1037,13 +1037,13 @@ const App: React.FC = () => {
                 <nav className="relative">
                      <button 
                         onClick={() => setIsMenuOpen(!isMenuOpen)} 
-                        className="text-white p-2 rounded-full hover:bg-white/10 transition focus:outline-none"
+                        className="text-white p-1.5 rounded-full hover:bg-white/10 transition focus:outline-none"
                         aria-label="Menu"
                     >
                         {isMenuOpen ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                         ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                         )}
                     </button>
 
