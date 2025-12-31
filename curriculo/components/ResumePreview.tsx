961b1e3e8291ad1597369d7de783a8e3d6eabcc3
mysaelsantos <167044211+mysaelsantos@users.noreply.github.com>
2025-12-31 13:22:39 -0300

    Update ResumePreview.tsx

--- curriculo/components/ResumePreview.tsx
+++ curriculo/components/ResumePreview.tsx
@@ -18,12 +18,14 @@ export const QR_CONFIG = {
         'template-classic': { 
             bottom: 35, 
             right: 25,
-            safetyPadding: 15 
+            safetyPadding: 15, 
+            overrideSpacer: { width: 230, height: 100 }
         },
         'template-minimalist': { 
             bottom: 30, 
             right: 25,
-            safetyPadding: 30 
+            safetyPadding: 30, 
+            overrideSpacer: { width: 230, height: 100 }
         },
     }
 };
